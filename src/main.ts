7c663a06bf9a258363ab41f7056e66e59841cc62
Patrick Niyogitare <patrickniyogitare28@gmail.com>
2023-12-06 21:08:41 +0200

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   const options = new DocumentBuilder()
     .setTitle('GreenHills_Academy')
